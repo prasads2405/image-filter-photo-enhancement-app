ad4e0c1d8abfb0cb029786d6f4aa78ac7044e45a
student <prasad.sivaraj@in.bosch.com>
2022-10-31 14:46:57 +0000

    Maven setup for image enhancement app

---
+++ image-enhancement-app/src/main/java/com/softmaxsolutions/image/enhancement/app/ImageEnhancementAppApplication.java
@@ -0,0 +1,13 @@
+package com.softmaxsolutions.image.enhancement.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageEnhancementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageEnhancementAppApplication.class, args);
+	}
+
+}
---
+++ image-enhancement-app/src/test/java/com/softmaxsolutions/image/enhancement/app/ImageEnhancementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softmaxsolutions.image.enhancement.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageEnhancementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
